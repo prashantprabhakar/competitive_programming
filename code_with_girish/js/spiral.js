aeb9cf03a21c48d99f5fb94b13752ea6586e51d5
prashant prabhakar singh <itsme.pps.aps@gmail.com>
2020-06-02 23:55:30 +0530

    Added driver fn

--- code_with_girish/js/spiral.js
+++ code_with_girish/js/spiral.js
@@ -1,8 +1,7 @@
 
-let sprial = []
-function printSpiral(matrix, firstRow=0, firstCol=0, lastRow=matrix.length-1, lastCol=matrix[0].length-1) {
+function printSpiral(matrix, sprial=[], firstRow=0, firstCol=0, lastRow=matrix.length-1, lastCol=matrix[0].length-1) {
 
-    if(firstRow > lastRow || firstCol > lastCol) return
+    if(firstRow > lastRow || firstCol > lastCol) return sprial
 
     // print first row
     if(firstRow <= lastRow) {
@@ -37,13 +36,21 @@ function printSpiral(matrix, firstRow=0, firstCol=0, lastRow=matrix.length-1, la
     lastRow--
     firstCol++
     lastCol--
-    printSpiral(matrix, firstRow, firstCol, lastRow, lastCol)
+    return printSpiral(matrix, sprial, firstRow, firstCol, lastRow, lastCol)
 }
 
 
-printSpiral([[1,2,3, 4, 20, 21, 22 ], [5,6, 7, 8, 23, 24, 25 ], [9, 10, 11, 12, 26, 27, 28 ], [13,14,15,16, 29, 30, 31 ], [17, 18, 19, 20, 32, 33,34 ]])
-console.log(sprial.length)
-console.log(sprial)
+function driver(matrix) {
+    if(!matrix.length) return []
+    if(!matrix[0].length) return []
+    return printSpiral(matrix)
+}
+
+driver([[1,2,3, 4, 20, 21, 22 ], [5,6, 7, 8, 23, 24, 25 ], [9, 10, 11, 12, 26, 27, 28 ], [13,14,15,16, 29, 30, 31 ], [17, 18, 19, 20, 32, 33,34 ]])
+
+console.log(driver([]))
+console.log(driver([[], [], []]))
+
 
 /**
  * [1  2  3  4  20 21 22 40]
