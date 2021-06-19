3d637509f06c5ad60a32848fb1d363078c927163
prashant.prabhakar <prashant.prabhakar@sofocle.com>
2021-06-19 12:05:25 +0530

    3 sum

---
+++ leet_code/15.3sum.js
@@ -0,0 +1,57 @@
+/**
+ * url:https://leetcode.com/problems/3sum/
+ * tag: leetcod e
+ */
+
+/**
+ * statement: Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ */
+
+// O(n^2) - not working
+function threeSum(arr, targetSum=0) {
+  if(arr.length < 3) return []
+  let results = []
+  let solutions = {}
+  // O(n) here
+  for(let i=0; i<arr.length; i++) {
+    let target = targetSum - arr[i]
+    // O(n) here
+    let matchingPairs = _findNumberWithGivenSum(arr, target, i)
+    // O(k)  here; where k is possible solution of _findNumberWithGivenSum
+    for(let j=0; j< matchingPairs.length; j++) {
+      // O(3) here
+      let key = [arr[i], matchingPairs[j][0], matchingPairs[j][1]].sort((a,b) => a-b).reduce((i,result) => `${result}_${i}`, '')
+      if(solutions.hasOwnProperty(key)) continue;
+      results.push([
+        arr[i],
+        matchingPairs[j][0],
+        matchingPairs[j][1]
+      ])
+      solutions[key] = true
+    }
+  }
+  return  results
+}
+
+function _findNumberWithGivenSum(arr, target, ignoreIndex=-1) {
+  let numIndex = {}
+  let result =  []
+  for(let i=0; i<arr.length; i++) {
+    if(i==ignoreIndex) continue
+    let requiedNum = target - arr[i]
+    if(numIndex.hasOwnProperty(requiedNum)) {
+      result.push([requiedNum, arr[i]])
+    }
+    numIndex[arr[i]] = i
+  }
+  return result;
+}
+
+
+// console.log(threeSum([-1,0,1,2,-1,-4]))
+// console.log(threeSum([0,0]))
+// console.log(threeSum([-1, -9]))
+// console.log(threeSum([-1,1,0])) // [[-1,0,1]]
+// console.log(threeSum([1,2,-2,-1])) // []
+console.log(threeSum([-1,0,1,2,-1,-4,-2,-3,3,0,4]))
+//console.log(_findNumberWithGivenSum([-1,0,1,2,-1,-4,-2,-3,3,0,4], 1))
