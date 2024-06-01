fae5b5e84e3a50378201a3a206ad74560637f6a6
prashant.prabhakar <prashantprabhakar123@gmail.com>
2024-06-01 21:47:30 +0530

    daily code

---
+++ leet_code/3110.score-of-a-string.js
@@ -0,0 +1,13 @@
+/**
+ * https://leetcode.com/problems/score-of-a-string/description/
+ * Difficulty: Easy
+ * 
+ */
+
+var scoreOfString = function(s) {
+    let score = 0;
+    for(let i=0; i<s.length-1; i++) {
+        score += Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1))
+    }
+    return score
+};
